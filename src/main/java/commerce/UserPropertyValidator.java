b75451f590031ffc72b0b4f4f2500ee8a5bcc8bb
Gyuwon Yi <gyuwon@live.com>
2024-11-15 14:32:35 +0900

    강화된 비밀번호 규칙을 구현한다
    회원 가입 시 비밀번호가 연속된 4개 이상의 문자를 포함하는지 검사한다.

--- src/main/java/commerce/UserPropertyValidator.java
+++ src/main/java/commerce/UserPropertyValidator.java
@@ -14,6 +14,20 @@ public class UserPropertyValidator {
     }
 
     public static boolean isPasswordValid(String password) {
-        return password != null && password.length() >= 8;
+        return password != null
+            && password.length() >= 8
+            && contains4SequentialCharacters(password) == false;
+    }
+
+    private static boolean contains4SequentialCharacters(String s) {
+        for (int i = 0; i < s.length() - 3; i++) {
+            if (s.charAt(i) + 1 == s.charAt(i + 1) &&
+                s.charAt(i + 1) + 1 == s.charAt(i + 2) &&
+                s.charAt(i + 2) + 1 == s.charAt(i + 3)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
