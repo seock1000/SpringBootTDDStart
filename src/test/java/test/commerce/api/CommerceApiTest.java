f01c117e2107f61ea60dccc2e3d581055f9e3c4f
Gyuwon Yi <gyuwon@live.com>
2025-01-28 00:53:19 +0900

    테스트 클래스 애노테이션을 추가하고 적용한다

---
+++ src/test/java/test/commerce/api/CommerceApiTest.java
@@ -0,0 +1,18 @@
+package test.commerce.api;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import commerce.CommerceApiApp;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(
+    classes = CommerceApiApp.class,
+    webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
+public @interface CommerceApiTest {
+}
