cc487994d6ecf0ec68e29b073835d917635356dc
Gyuwon Yi <gyuwon@live.com>
2025-02-12 18:58:32 +0900

    CreateSellerCommand 명령에 이메일이 지정되는지 검사
    테스트 시나리오
    - email 속성이 지정되지 않으면 400 Bad Request 상태코드를 반환한다

--- src/main/java/commerce/api/controller/SellerSignUpController.java
+++ src/main/java/commerce/api/controller/SellerSignUpController.java
@@ -1,14 +1,20 @@
 package commerce.api.controller;
 
+import commerce.command.CreateSellerCommand;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public record SellerSignUpController() {
 
     @PostMapping("/seller/signUp")
-    ResponseEntity<?> signUp() {
-        return ResponseEntity.noContent().build();
+    ResponseEntity<?> signUp(@RequestBody CreateSellerCommand command) {
+        if (command.email() == null) {
+            return ResponseEntity.badRequest().build();
+        } else {
+            return ResponseEntity.noContent().build();
+        }
     }
 }
