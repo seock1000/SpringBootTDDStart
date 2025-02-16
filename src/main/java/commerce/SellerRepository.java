a17551ed25de83c0fbe482a848d087afc521858c
Gyuwon Yi <gyuwon@live.com>
2025-02-16 12:42:16 +0900

    판매자 접근 토큰 발행 API 실패 시나리오 구현
    테스트 시나리오
    - 존재하지 않는 이메일 주소가 사용되면 400 Bad Request 상태코드를
      반환한다
    - 잘못된 비밀번호가 사용되면 400 Bad Request 상태코드를 반환한다

--- src/main/java/commerce/SellerRepository.java
+++ src/main/java/commerce/SellerRepository.java
@@ -1,6 +1,10 @@
 package commerce;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SellerRepository extends JpaRepository<Seller, Long> {
+
+    Optional<Seller> findByEmail(String email);
 }
