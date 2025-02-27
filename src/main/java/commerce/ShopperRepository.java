61f7d654c6672825771ebd93f7ed3207da6f09fc
Gyuwon Yi <gyuwon@live.com>
2025-02-28 03:30:13 +0900

    구매자 접근 토큰 발행 API 구현을 완료한다
    테스트 시나리오
    - 접근 토큰은 JWT 형식을 따른다
    - 존재하지 않는 이메일 주소가 사용되면 400 Bad Request 상태코드를
      반환한다
    - 잘못된 비밀번호가 사용되면 400 Bad Request 상태코드를 반환한다

--- src/main/java/commerce/ShopperRepository.java
+++ src/main/java/commerce/ShopperRepository.java
@@ -1,6 +1,10 @@
 package commerce;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ShopperRepository extends JpaRepository<Shopper, Long> {
+
+    Optional<Shopper> findByEmail(String email);
 }
--- src/test/java/test/commerce/api/shopper/issuetoken/POST_specs.java
+++ src/test/java/test/commerce/api/shopper/issuetoken/POST_specs.java
@@ -10,8 +10,10 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
 import test.commerce.api.CommerceApiTest;
 
+import static java.util.Objects.requireNonNull;
 import static org.assertj.core.api.Assertions.assertThat;
 import static test.commerce.EmailGenerator.generateEmail;
+import static test.commerce.JwtAssertions.conformsToJwtFormat;
 import static test.commerce.PasswordGenerator.generatePassword;
 import static test.commerce.UsernameGenerator.generateUsername;
 
@@ -44,5 +46,76 @@ public class POST_specs {
         assertThat(response.getStatusCode().value()).isEqualTo(200);
         assertThat(response.getBody()).isNotNull();
         assertThat(response.getBody().accessToken()).isNotNull();
+    }
+
+    @Test
+    void 접근_토큰은_JWT_형식을_따른다(
+        @Autowired TestRestTemplate client
+    ) {
+        // Arrange
+        String email = generateEmail();
+        String password = generatePassword();
+
+        client.postForEntity(
+            "/shopper/signUp",
+            new CreateShopperCommand(email, generateUsername(), password),
+            Void.class
+        );
+
+        // Act
+        ResponseEntity<AccessTokenCarrier> response = client.postForEntity(
+            "/shopper/issueToken",
+            new IssueShopperToken(email, password),
+            AccessTokenCarrier.class
+        );
+
+        // Assert
+        String actual = requireNonNull(response.getBody()).accessToken();
+        assertThat(actual).satisfies(conformsToJwtFormat());
+    }
+
+    @Test
+    void 존재하지_않는_이메일_주소가_사용되면_400_Bad_Request_상태코드를_반환한다(
+        @Autowired TestRestTemplate client
+    ) {
+        // Arrange
+        String email = generateEmail();
+        String password = generatePassword();
+
+        // Act
+        ResponseEntity<Void> response = client.postForEntity(
+            "/shopper/issueToken",
+            new IssueShopperToken(email, password),
+            Void.class
+        );
+
+        // Assert
+        assertThat(response.getStatusCode().value()).isEqualTo(400);
+    }
+
+    @Test
+    void 잘못된_비밀번호가_사용되면_400_Bad_Request_상태코드를_반환한다(
+        @Autowired TestRestTemplate client
+    ) {
+        // Arrange
+        String email = generateEmail();
+        String password = generatePassword();
+        String wrongPassword = generatePassword();
+
+        client.postForEntity(
+            "/shopper/signUp",
+            new CreateShopperCommand(email, generateUsername(), password),
+            Void.class
+        );
+
+        // Act
+        ResponseEntity<Void> response = client.postForEntity(
+            "/shopper/issueToken",
+            new IssueShopperToken(email, wrongPassword),
+            Void.class
+        );
+
+        // Assert
+        assertThat(response.getStatusCode().value()).isEqualTo(400);
     }
 }
