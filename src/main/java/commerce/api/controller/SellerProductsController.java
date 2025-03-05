5b38ab2fbcb58a861dc2dce5aa3e7b6c830227aa
Gyuwon Yi <gyuwon@live.com>
2025-03-05 14:38:36 +0900

    판매자 상품 등록 API에 이미지 URI 검사 추가
    테스트 시나리오
    - imageUri 속성이 URI 형식을 따르지 않으면 400 Bad Request 상태코드를
      반환한다

--- src/main/java/commerce/api/controller/SellerProductsController.java
+++ src/main/java/commerce/api/controller/SellerProductsController.java
@@ -1,23 +1,40 @@
 package commerce.api.controller;
 
+import java.net.URI;
 import java.security.Principal;
 import java.util.UUID;
 
 import commerce.SellerRepository;
+import commerce.command.RegisterProductCommand;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public record SellerProductsController(SellerRepository repository) {
 
     @PostMapping("/seller/products")
-    ResponseEntity<?> registerProduct(Principal user) {
+    ResponseEntity<?> registerProduct(
+        Principal user,
+        @RequestBody RegisterProductCommand command
+    ) {
         UUID id = UUID.fromString(user.getName());
         if (repository.findById(id).isEmpty()) {
             return ResponseEntity.status(403).build();
+        } else if (isValidUri(command.imageUri()) == false) {
+            return ResponseEntity.badRequest().build();
         }
 
         return ResponseEntity.status(201).build();
+    }
+
+    private boolean isValidUri(String value) {
+        try {
+            URI uri = URI.create(value);
+            return uri.getHost() != null;
+        } catch (IllegalArgumentException exception) {
+            return false;
+        }
     }
 }
