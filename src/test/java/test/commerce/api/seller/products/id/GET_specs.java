8646f8eb66df8b5facd974b9dacebe94a25b1e4f
Gyuwon Yi <gyuwon@live.com>
2025-03-13 20:15:10 +0900

    판매자 상품 조회 API가 상품 등록 시각을 설정한다
    테스트 시나리오
    - 상품 등록 시각을 올바르게 반환한다

--- src/test/java/test/commerce/api/seller/products/id/GET_specs.java
+++ src/test/java/test/commerce/api/seller/products/id/GET_specs.java
@@ -1,5 +1,6 @@
 package test.commerce.api.seller.products.id;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 import commerce.command.RegisterProductCommand;
@@ -11,7 +12,10 @@ import org.springframework.http.ResponseEntity;
 import test.commerce.api.CommerceApiTest;
 import test.commerce.api.TestFixture;
 
+import static java.time.ZoneOffset.UTC;
+import static java.time.temporal.ChronoUnit.SECONDS;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.within;
 import static test.commerce.ProductAssertions.isDerivedFrom;
 import static test.commerce.RegisterProductCommandGenerator.generateRegisterProductCommand;
 
@@ -131,5 +135,25 @@ public class GET_specs {
 
         // Assert
         assertThat(actual).satisfies(isDerivedFrom(command));
+    }
+
+    @Test
+    void 상품_등록_시각을_올바르게_반환한다(
+        @Autowired TestFixture fixture
+    ) {
+        // Arrange
+        fixture.createSellerThenSetAsDefaultUser();
+        LocalDateTime referenceTime = LocalDateTime.now(UTC);
+        UUID id = fixture.registerProduct();
+
+        // Act
+        SellerProductView actual = fixture.client().getForObject(
+            "/seller/products/" + id,
+            SellerProductView.class
+        );
+
+        // Assert
+        assertThat(actual.registeredTimeUtc())
+            .isCloseTo(referenceTime, within(1, SECONDS));
     }
 }
