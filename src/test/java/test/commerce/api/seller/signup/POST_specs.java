132935e0d5a0f674bfb51b951e5d4ea22d8bd94e
seock1000 <tjrcjs0705@gmail.com>
2025-06-14 16:48:53 +0900

    [FEAT] 판매자 회원가입 API password 속성이 올바르지 않은 케이스 테스트 작성

--- src/test/java/test/commerce/api/seller/signup/POST_specs.java
+++ src/test/java/test/commerce/api/seller/signup/POST_specs.java
@@ -190,4 +190,30 @@ public class POST_specs {
         // Assert
         assertThat(response.getStatusCode().value()).isEqualTo(400);
     }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+        "",
+        "pass",
+        "pass123",
+    })
+    void password_속성이_올바른_형식을_따르지_않으면_400_BAD_REQUEST_응답을_반환한다(
+        String password,
+        @Autowired TestRestTemplate client
+    ) {
+        // Arrange
+        CreateSellerCommand command = new CreateSellerCommand(
+            "seller@test.com",
+            "seller",
+            password // 잘못된 형식의 password
+        );
+        // Act
+        ResponseEntity<Void> response = client.postForEntity(
+            "/seller/signup",
+            command, // 요청 본문
+            Void.class // 응답 본문
+        );
+        // Assert
+        assertThat(response.getStatusCode().value()).isEqualTo(400);
+    }
 }
