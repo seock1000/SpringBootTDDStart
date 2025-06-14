c4d1a653b1dd6ff06a0caeb54ed9dc43437543ea
seock1000 <tjrcjs0705@gmail.com>
2025-06-14 15:56:00 +0900

    [FEAT] 판매자 회원가입 API email 속성 지정하지 않은 케이스 테스트 작성

--- src/test/java/test/commerce/api/seller/signup/POST_specs.java
+++ src/test/java/test/commerce/api/seller/signup/POST_specs.java
@@ -39,4 +39,26 @@ public class POST_specs {
         // Assert
         assertThat(response.getStatusCode().value()).isEqualTo(204);
     }
+
+    @Test
+    void email_속성이_지정되지_않으면_400_BAD_REQUEST_응답을_반환한다(
+        @Autowired TestRestTemplate client
+    ) {
+        // Arrange
+        CreateSellerCommand command = new CreateSellerCommand(
+            null, // email 속성 없음
+            "seller",
+            "password"
+        );
+
+        // Act
+        ResponseEntity<Void> response = client.postForEntity(
+            "/seller/signup",
+            command, // 요청 본문
+            Void.class // 응답 본문
+        );
+
+        // Assert
+        assertThat(response.getStatusCode().value()).isEqualTo(400);
+    }
 }
