fc6db256af677ffe58fc05d93d4efa6684a13611
seock1000 <tjrcjs0705@gmail.com>
2025-06-16 22:43:52 +0900

    [FEAT] 판매자 회원가입 API 올바른 비밀번호 암호화 구현

--- src/main/java/commerce/api/SecurityConfiguration.java
+++ src/main/java/commerce/api/SecurityConfiguration.java
@@ -2,7 +2,11 @@ package commerce.api;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfiguration {
@@ -10,5 +14,15 @@ public class SecurityConfiguration {
     @Bean
     Pbkdf2PasswordEncoder passwordEncoder() {
         return Pbkdf2PasswordEncoder.defaultsForSpringSecurity_v5_8();
+    }
+
+    @Bean
+    DefaultSecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+            .csrf(AbstractHttpConfigurer::disable) // CSRF 보호 비활성화
+            .authorizeHttpRequests(requests -> requests
+                .requestMatchers("/seller/signup").permitAll() // /seller/signup 경로는 인증 없이 접근 허용
+            )
+            .build();
     }
 }
--- src/main/java/commerce/api/controller/SellerSignUpController.java
+++ src/main/java/commerce/api/controller/SellerSignUpController.java
@@ -6,12 +6,16 @@ import commerce.command.CreateSellerCommand;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public record SellerSignUpController(SellerRepository sellerRepository) {
+public record SellerSignUpController(
+    PasswordEncoder passwordEncoder,
+    SellerRepository sellerRepository
+) {
 
     private static final String EMAIL_REGEX = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$";
     private static final String USERNAME_REGEX = "^[a-zA-Z0-9_-]{3,}$";
@@ -24,9 +28,11 @@ public record SellerSignUpController(SellerRepository sellerRepository) {
             return ResponseEntity.badRequest().build();
         }
 
+        String hashedPassword = passwordEncoder.encode(command.password());
         Seller seller = new Seller();
         seller.setEmail(command.email());
         seller.setUsername(command.username());
+        seller.setHashedPassword(hashedPassword);
         try {
             sellerRepository.save(seller);
         } catch (DataIntegrityViolationException e) {
