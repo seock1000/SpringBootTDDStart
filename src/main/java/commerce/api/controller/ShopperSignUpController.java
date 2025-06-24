d79109c8f088b56a3aef185f826d8d4f5a8a6d33
seock1000 <tjrcjs0705@gmail.com>
2025-06-24 21:43:13 +0900

    [FEAT] 구매자 회원가입 - password 미지정 테스트 및 구현

--- src/main/java/commerce/api/controller/ShopperSignUpController.java
+++ src/main/java/commerce/api/controller/ShopperSignUpController.java
@@ -24,6 +24,7 @@ public record ShopperSignUpController() {
 
     private static boolean isCommandValid(CreateShopperCommand command) {
         return isEmailValid(command.email())
-            && isUsernameValid(command.username());
+            && isUsernameValid(command.username())
+            && command.password() != null;
     }
 }
