f9f11c5859ed3462a9645e7226410a068722c9e8
seock1000 <tjrcjs0705@gmail.com>
2025-06-24 20:59:59 +0900

    [FEAT] 구매자 회원가입 - 올바른 요청 테스트 작성

---
+++ src/test/java/test/commerce/api/shopper/signup/POST_specs.java
@@ -0,0 +1,41 @@
+package test.commerce.api.shopper.signup;
+
+import commerce.command.CreateShopperCommand;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import test.CommerceApiTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static test.commerce.EmailGenerator.generateEmail;
+import static test.commerce.PasswordGenerator.generatePassword;
+import static test.commerce.UsernameGenerator.generateUsername;
+
+@CommerceApiTest
+@DisplayName("/shopper/signup")
+public class POST_specs {
+
+    @Test
+    void 올바르게_요청하면_204_NO_CONTENT_응답을_반환한다(
+        @Autowired TestRestTemplate client // Client 역할
+    ) {
+        // Arrange
+        CreateShopperCommand command = new CreateShopperCommand(
+            generateEmail(),
+            generateUsername(),
+            generatePassword()
+        );
+
+        // Act
+        ResponseEntity<Void> response = client.postForEntity(
+            "/shopper/signup",
+            command, // 요청 본문
+            Void.class // 응답 본문
+        );
+
+        // Assert
+        assertThat(response.getStatusCode().value()).isEqualTo(204);
+    }
+}
