9ed844a66915dceb24783ca3adb6958a84b3c3a7
seock1000 <tjrcjs0705@gmail.com>
2025-06-25 23:25:12 +0900

    [FEAT] 판매자 정보 조회 다른 판매자의 식별자는 서로 다름 구현

--- src/main/java/commerce/api/controller/SellerMeController.java
+++ src/main/java/commerce/api/controller/SellerMeController.java
@@ -1,12 +1,16 @@
 package commerce.api.controller;
 
+import commerce.view.SellerMeView;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 public record SellerMeController() {
 
     @GetMapping("/seller/me")
-    void me() {
+    SellerMeView me() {
+        return new SellerMeView(UUID.randomUUID(), null, null);
     }
 }
