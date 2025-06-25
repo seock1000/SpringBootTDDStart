8632ba0fbcf12902617adbb2bf51bd56c5bb7aee
seock1000 <tjrcjs0705@gmail.com>
2025-06-25 23:44:36 +0900

    [FEAT] 판매자 정보 조회 올바른 기본 정보 구현

--- src/main/java/commerce/SellerRepository.java
+++ src/main/java/commerce/SellerRepository.java
@@ -3,8 +3,10 @@ package commerce;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 public interface SellerRepository extends JpaRepository<Seller, Long> {
 
     Optional<Seller> findByEmail(String email);
+    Optional<Seller> findById(UUID id);
 }
