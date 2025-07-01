e9d87f1741e9ccf21b7ce9eafb883757d8b4c7a9
seock1000 <tjrcjs0705@gmail.com>
2025-07-01 21:38:03 +0900

    [DOCS] 상품 판매자 상품 등록 - 올바르지 않은 이미지 url 구현

--- src/main/java/commerce/api/controller/SellerProductController.java
+++ src/main/java/commerce/api/controller/SellerProductController.java
@@ -1,10 +1,13 @@
 package commerce.api.controller;
 
 import commerce.SellerRepository;
+import commerce.command.RegisterProductCommand;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URI;
 import java.security.Principal;
 import java.util.UUID;
 
@@ -15,12 +18,24 @@ public record SellerProductController(
 
     @PostMapping("/seller/products")
     public ResponseEntity<?> registerProduct(
-        Principal user
+        Principal user,
+        @RequestBody RegisterProductCommand command
     ) {
         UUID id = UUID.fromString(user.getName());
         if(repository.findById(id).isEmpty()) {
             return ResponseEntity.status(403).build();
+        } else if(!isValidUrl(command.imgUri())) {
+            return ResponseEntity.badRequest().build();
         }
         return ResponseEntity.status(201).build();
+    }
+
+    private boolean isValidUrl(String value) {
+        try {
+            URI uri = URI.create(value);
+            return uri.getHost() != null;
+        } catch (IllegalArgumentException e) {
+            return false;
+        }
     }
 }
