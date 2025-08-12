dc2d0b71a3bcce9755b43f3605e4ec754d24e666
seock1000 <tjrcjs0705@gmail.com>
2025-08-12 19:58:11 +0900

    테스트 설정 구성

--- src/main/java/commerce/Product.java
+++ src/main/java/commerce/Product.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -15,6 +17,7 @@ import java.util.UUID;
 @Entity
 @Getter
 @Setter
+@Table(indexes = @Index(columnList = "sellerId"))
 public class Product {
 
     @Id
