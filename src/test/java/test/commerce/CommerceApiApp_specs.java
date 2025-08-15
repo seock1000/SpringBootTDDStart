1450a702bb460331df1c87ad02319281afcd5d83
seock1000 <tjrcjs0705@gmail.com>
2025-08-15 20:28:05 +0900

    PasswordEncoder Test Bean 등록 테스트 성능 최적화

--- src/test/java/test/commerce/CommerceApiApp_specs.java
+++ src/test/java/test/commerce/CommerceApiApp_specs.java
@@ -2,12 +2,30 @@ package test.commerce;
 
 import commerce.CommerceApiApp;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+import test.commerce.api.TestFixture;
+import test.commerce.api.TestFixtureConfiguration;
 
-@SpringBootTest(classes = CommerceApiApp.class)
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(classes = {
+    CommerceApiApp.class,
+    TestFixtureConfiguration.class,
+    PasswordEncoderConfiguration.class
+})
 public class CommerceApiApp_specs {
 
     @Test
     void 컨텍스트가_초기화된다() {
+    }
+
+    @Test
+    void PasswordEncoder빈이_올바르게_설정된다(
+        @Autowired PasswordEncoder actual
+        ) {
+        assertThat(actual).isInstanceOf(Pbkdf2PasswordEncoder.class);
     }
 }
